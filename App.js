a8530f07440f357aea2b933416dd33603cc0b483
haimnewclass <56435506+haimnewclass@users.noreply.github.com>
2022-05-16 13:15:08 +0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button, ScrollView } from 'react-native';
+import axios from 'axios';
+import {useState} from 'react';
 
 export default function App() {
+  const [arr,setArr] = useState([]);
+  console.log('start');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>HTTP 3</Text>
       <StatusBar style="auto" />
+      <View  style={{marginTop:15}}>
+    
+        
+        <Button title='Get HTTP2' style={{marginTop:15}} onPress={ ()=>{
+          alert('call to http2');
+          console.log('call to http');
+
+          axios.get('https://jsonplaceholder.typicode.com/todos')
+          .then((response)=>{
+              console.log(response.data);
+              setArr(response.data);
+          }).catch((err)=>{
+            alert(err.response);
+          })
+    
+
+        }} />
+      </View>
+      <ScrollView>
+
+      {arr?.map((x)=>{
+
+        return (<View>
+          <Text>{x?.title}</Text>
+        </View>)
+      })}
+
+      </ScrollView>
     </View>
   );
 }
